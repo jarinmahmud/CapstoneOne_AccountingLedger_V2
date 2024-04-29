7b157f21c833ae22b9e9a232877dc7624e70495d
Nuhirath Rafthia <nr2793@columbia.edu>
2024-04-29 11:26:37 -0400

    Create starter code for building home screen menu

--- src/main/java/com/ps/Main.java
+++ src/main/java/com/ps/Main.java
@@ -1,7 +1,23 @@
 package com.ps;
 
+import java.util.Scanner;
+
 public class Main {
+    // use scanner for input throughout code
+    static Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String selection;
+        // Home Screen
+        do {
+            System.out.println("Welcome To The Accounting Ledger.");
+            System.out.println("D) Add Deposit");
+            System.out.println("P) Make Payment (Debit)");
+            System.out.println("L) Ledger Screen");
+            System.out.println("X) Exit");
+            System.out.print("Please make a selection: ");
+            selection = scanner.next();
+        } while (!selection.equals("X"));
+        System.out.println("Thank you for using the Ledger Application!");
     }
 }
