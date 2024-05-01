9adb35245c1e96cf6a77470beb878af1622f1a50
Nuhirath Rafthia <nr2793@columbia.edu>
2024-05-01 13:33:29 -0400

    Create CustomSearch class and outline the template to envelop structure for performing search functionalities

---
+++ src/main/java/com/ps/CustomSearch.java
@@ -0,0 +1,50 @@
+package com.ps;
+
+public class CustomSearch {
+    // Simple Search Customizations
+    public static void monthToDate(){
+
+    }
+
+    public static void previousMonth(){
+
+    }
+
+    public static void yearToDate(){
+
+    }
+
+    public static void previousYear(){
+
+    }
+
+    public static void searchByVendor(){
+
+    }
+
+    // Deeper Search Customization - BONUS
+    public static void randomValueSearch(){
+
+    }
+
+    public static void startDateSearch(){
+
+    }
+
+    public static void endDateSearch(){
+
+    }
+
+    public static void descriptionSearch(){
+
+    }
+
+    public static void vendorSearch(){
+
+    }
+
+    public static void amountSearch(){
+
+    }
+
+}
